7ccc38e3c2b9ee7d087c82951700b882be98a341
katrinani <anisimovakaterina328@gmail.com>
2024-04-28 06:23:30 +0300

    карта

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import FormCreatePoint from "./components/FormCreatePoint/formCreatePoint";
 import Map from "./components/Map/Map";
 
 function App() {
-  const location = { center: [37.95, 55.65], zoom: 10 };
-  const apiKey = "47e9428b-e698-4791-98ce-87001909f7fb";
+  const location = { center: [37.617299, 55.745826], zoom: 10 };
+  const apiKey = "f76e8033-445d-48b9-8805-b6987c78678d";
   return (
     <div className="App bg-light-subtle">
       <div className="position-absolute translate-middle top-50 start-50 w-75 h-75 d-flex flex-row">
         <FormCreatePoint />
-        <Map location={location} apiKey={apiKey} />
+          <Map location={location.center} apiKey={apiKey} />
       </div>
     </div>
   );
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import {
   YMap,
   YMapDefaultSchemeLayer,
@@ -7,16 +8,63 @@ import {
 } from "ymap3-components";
 
 const Map = (props) => {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const testData = [
+      { name: "Point 1", latitude: 55.755826, longitude: 37.617299 },
+      { name: "Point 2", latitude: 55.755826, longitude: 37.627299 },
+      { name: "Point 3", latitude: 55.745826, longitude: 37.617299 },
+    ];
+
+    const newMarkers = testData.map((point) => ({
+      geometry: [point.longitude, point.latitude],
+      properties: {
+        balloonContent: point.name,
+      },
+    }));
+    setMarkers(newMarkers);
+  }, []);
+
+// useEffect(() => {
+  //   fetch("https://your-backend-url/points")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const newMarkers = data.map((point) => ({
+  //           geometry: [point.longitude, point.latitude],
+  //           properties: {
+  //             balloonContent: point.name,
+  //           },
+  //         }));
+  //         setMarkers(newMarkers);
+  //       })
+  //       .catch((error) => console.error("Error fetching points:", error));
+  // }, []);
+
   return (
-    <div className="map w-50 h-100 px-2">
-      <YMapComponentsProvider apiKey={props.apiKey}>
-        <YMap location={props.location}>
-          <YMapDefaultSchemeLayer />
-          <YMapDefaultFeaturesLayer />
-        </YMap>
-      </YMapComponentsProvider>
-    </div>
+      <div className="map w-50 h-100 px-2">
+        <YMapComponentsProvider apiKey={props.apiKey}>
+          <YMap defaultState={{ center: props.location, zoom: 10 }}>
+            <YMapDefaultSchemeLayer />
+            <YMapDefaultFeaturesLayer />
+            {markers && markers.map((marker, index) => (
+                <YMapDefaultMarker
+                    key={index}
+                    coordinates={marker.geometry}
+                    properties={marker.properties}
+                />
+            ))}
+          </YMap>
+        </YMapComponentsProvider>
+      </div>
   );
 };
 
 export default Map;
+
+
+
+
+
+
+
