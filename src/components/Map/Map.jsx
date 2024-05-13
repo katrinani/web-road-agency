34cf4c5bcd71750995c5b3f93c0d01b054231a59
katrinani <anisimovakaterina328@gmail.com>
2024-05-13 17:01:09 +0300

    Обработка нажатия на карту

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,16 +7,24 @@ import {
 } from "ymap3-components";
 import React from "react";
 
+
+const handleMarkerClick = (latitude, longitude, name) => {
+  console.log("Координаты маркера: ", latitude, longitude);
+  console.log("Название", name)
+};
+
+
 const Map = (props) => {
   return (
     <div className="map w-50 h-100 px-2">
       <YMapComponentsProvider apiKey={props.apiKey}>
-        <YMap location={props.location}>
+        <YMap location={props.location} >
           <YMapDefaultSchemeLayer />
           <YMapDefaultFeaturesLayer />
           {props.points_list &&
             props.points_list.map((marker, index) => (
               <YMapDefaultMarker
+                  onClick={() => handleMarkerClick(marker["Широта"], marker["Долгота"], marker["Название"])}
                 key={index}
                 coordinates={[marker["Широта"], marker["Долгота"]]}
                 title={marker["Название"]}
