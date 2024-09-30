52b278c1655a27349a3600589f0db8805146e501
katrina <anisimovakaterina328@gmail.com>
2024-09-30 14:52:45 +0500

    Подготовка окружения для деплоя

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import CrudPoints from "./helpers/Request/CrudPoints";
 
 
 function App() {
-  const apiKey = "f76e8033-445d-48b9-8805-b6987c78678d";
+  const apiKey = process.env.API_KEY;
   const [location, setLocation] = useState({ center: [55.163742, 61.400346], zoom: 11 });
   const [points_list, set_list] = useState([]);
   const [showModal, setShowModal] = useState(false);
--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,6 +1,8 @@
-export const url = "http://213.171.29.33:5139/api/v1/Web";
+const backend_url = process.env.BACKEND_URL;
+
+export const url = `${backend_url}/api/v1/Web`;
 export default url;
 
-export const URLForPhoto = "http://213.171.29.33:5139/api/v1/Web/files/";
+export const URLForPhoto = `${backend_url}/api/v1/Web/files/`;
 
 export const URLForUndo = "Icons/x-button.png";
